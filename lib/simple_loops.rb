8201bb47d3ab5303a0ddc992832a9ac675254c93
Kevin Glasgow <kglas001@gmail.com>
2020-09-04 21:37:03 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -23,7 +23,7 @@ def output_array(array)
   end
 end 
 
-def return_sting_array(array)
+def return_string_array(array)
   count = 0 
   new_array = []
   while count < array.length do 
@@ -32,13 +32,3 @@ def return_sting_array(array)
   end
   new_array
 end 
-
-def return_string_array(array)
-  count = 0
-  new_array = []
-  while count < array.length do
-    new_array.push(array[count].to_s) # OR new_array << array[count].to_s
-    count += 1
-  end
-  new_array
-end
